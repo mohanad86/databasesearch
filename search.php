ff7c1f6266f213f62c664ed418b06f788e116c01
mohanad86 <capmohanad@hotmail.com>
2016-10-29 22:52:12 +0300

    Adding the new search query

--- search.php
+++ search.php
@@ -6,14 +6,23 @@ if (!isset($_GET['q'])) {
 header('Content-type: text/json; charset=utf-8');
 $searchParam = '%' . $_GET['q'] . '%';
 $moment = microtime(true);
-//$stmt = $db->prepare('SELECT * FROM product WHERE (serial_number LIKE ? OR name LIKE ?) ORDER BY production_date DESC LIMIT 100');
+$stmt = $db->prepare('SELECT * FROM product WHERE (serial_number LIKE ? OR name LIKE ?) ORDER BY production_date DESC LIMIT 10');
+//if (!$stmt) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
+$countproducts = $stmt->fetchColumn();
+if ($countproducts > 0 ){
 
-$stmt = $db->prepare('SELECT * FROM product WHERE (serial_number LIKE ? OR name LIKE ?) ORDER BY name DESC LIMIT 10');
+	$stmt = $db->prepare('SELECT * FROM product WHERE (serial_number LIKE ? OR name LIKE ?)'
+				. ' ORDER BY production_date' . min(10, $countproducts));
+	$stmt->execute(array($searchParam, $searchParam));
+	$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	//var_dump($countproducts);
+}
 
 
 
 $stmt->execute(array($searchParam, $searchParam));
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//var_dump($products);
 $queryTime = microtime(true) - $moment;
 $output = array(
 	'query_time' => $queryTime,
